a5ce0500dd8513f57395246c425d96130181e053
Taine <taine.reader@gmail.com>
2021-07-24 11:56:52 +1200

    RGB Skybox
    Glyn

---
+++ Assets/Scripts/SkyBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBox : MonoBehaviour
+{
+    public Material skyboxMaterial;
+    private float lerp;
+    float timeLeft;
+    Color targetColor;
+    void Update() {
+        if (timeLeft <= Time.deltaTime) {
+            skyboxMaterial.SetColor("_SkyTint",targetColor);
+            
+            targetColor = new Color(Random.value, Random.value, Random.value,1);
+            timeLeft = 1.0f;
+        } else {
+            skyboxMaterial.SetColor("_SkyTint",Color.Lerp(skyboxMaterial.GetColor("_SkyTint"), targetColor, Time.deltaTime / timeLeft));
+        
+            timeLeft -= Time.deltaTime;
+        }
+    }
+}
