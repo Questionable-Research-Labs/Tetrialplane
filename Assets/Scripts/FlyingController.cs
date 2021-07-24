dd041e47edeecaa5960ccef8f9099803800e1cc8
Taine <taine.reader@gmail.com>
2021-07-24 13:55:02 +1200

    Naming conventions, what are those?

--- Assets/Scripts/FlyingController.cs
+++ Assets/Scripts/FlyingController.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using TreeEditor;
+//begone thot
 using Unity.VisualScripting;
 using UnityEngine;
 using Object = UnityEngine.Object;
