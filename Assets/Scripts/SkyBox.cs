61df3ebd9f9b80ca64c4391118cd7722900c59ec
Taine <taine.reader@gmail.com>
2021-07-24 16:36:28 +1200

    Noice Menu

--- Assets/Scripts/SkyBox.cs
+++ Assets/Scripts/SkyBox.cs
@@ -10,12 +10,12 @@ public class SkyBox : MonoBehaviour
     Color targetColor;
     void Update() {
         if (timeLeft <= Time.deltaTime) {
-            skyboxMaterial.SetColor("_SkyTint",targetColor);
+            skyboxMaterial.SetColor("_Tint",targetColor);
             
             targetColor = new Color(Random.value, Random.value, Random.value,1);
             timeLeft = 1.0f;
         } else {
-            skyboxMaterial.SetColor("_SkyTint",Color.Lerp(skyboxMaterial.GetColor("_SkyTint"), targetColor, Time.deltaTime / timeLeft));
+            skyboxMaterial.SetColor("_Tint", Color.Lerp(skyboxMaterial.GetColor("_Tint"), targetColor, Time.deltaTime / timeLeft));
         
             timeLeft -= Time.deltaTime;
         }
